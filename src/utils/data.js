561ae1ccfc6d76e4c1f6374b75a77676c3a7bb8e
felipeverger <felipeverger123@gmail.com>
2022-11-05 15:37:22 -0300

    refactorize el codigo de sidebar modularizandolo y haciendolo mas legible

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,3 +1,25 @@
+import { HiDocument } from 'react-icons/hi';
+import {AiOutlineUser} from 'react-icons/ai';
+import {IoIosAddCircle} from 'react-icons/io';
+
+export const sidebarLinks = [
+    {
+      icon: <AiOutlineUser className='sidebar-icon'/>,
+      text: 'Your profile',
+      to: '/user-profile'
+    },
+    {
+      icon: <HiDocument className='sidebar-icon'/>,
+      text: 'Your applications',
+      to: '/applications'
+    },
+    {
+      icon: <IoIosAddCircle className='sidebar-icon'/>,
+      text: 'Create application',
+      to: '/add-application'
+    },
+]
+
 export const userQuery = (email) => {
     const query = `*[_type == "user" && email == '${email}']`;
     return query;
